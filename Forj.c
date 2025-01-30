1d7cf99925b1f83992a93b973c16ef3db1dd4e2c
flintwinters <flintwinters.0@gmail.com>
2025-01-30 18:23:10 -0500

    + set/get type design behavior

--- Forj.c
+++ Forj.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
-#include "utils.c"
 typedef struct Atom Atom;
 typedef long long word;
+typedef Atom* (*Go)(Atom*, Atom*);
 
 struct Atom {
     Atom* n, *t;
     word w, r;
 };
-Atom* new() {return malloc(sizeof(Atom));}
-Atom* init(Atom* n, Atom* t) {
-    Atom* a = new();
-    a->n = n; a->t = t;
-    a->w = a->r = 0;
+Atom* new() {
+    Atom* a = malloc(sizeof(Atom));
+    *a = (Atom) {0, 0, 0, 0};
     return a;
 }
 Atom* del(Atom* a) {
@@ -22,8 +20,8 @@ Atom* del(Atom* a) {
     return 0;
 }
 Atom* ref(Atom* a) {if (a) {a->r++;} return a;}
-Atom* pushn(Atom* n) {return init(ref(n), 0);}
-Atom* pusht(Atom* t) {return init(0, ref(t));}
+Atom* nset(Atom* a, Atom* n) {del(a->n); a->n = ref(n); return a;}
+Atom* tset(Atom* a, Atom* t) {del(a->t); a->t = ref(t); return a;}
 Atom* is(Atom* a, Atom* t) {
     if (!a) {return 0;}
     if (a->t == t) {return a;}
@@ -36,5 +34,45 @@ Atom* as(Atom* a, Atom* t) {
     if (n) {return n;}
     return as(a->t, t);
 }
+Atom* last(Atom* a) {
+    if (a->n) {return last(a->n);}
+    return a;
+}
+Atom* append(Atom* a, Atom* t) {return tset(nset(last(a), new())->n, t);}
+Atom* prepend(Atom* n, Atom* t) {return tset(nset(new(), n), t);}
 
-int main() {}
+Atom* ptr;
+Atom* val(Atom* a, Atom* e) {
+    e = prepend(e, a->t);
+    e->w = a->w;
+    return ref(e);
+}
+Atom* get(Atom* a, Atom* e) {
+    e = prepend(e, ptr);
+    e->w = (word) a->t;
+    return ref(e);
+}
+Atom* set(Atom* a, Atom* e) {
+    tset(a, e->t);
+    return e;
+}
+int main() {
+    Atom* T = new();
+    Atom* Tget = ref(append(T, new()));
+    Atom* goer = ref(tset(Tget->t, T));
+    Atom* valu = ref(tset(new(), T));
+    ptr = ref(tset(new(), valu));
+    append(valu, goer)->w = (word) val;
+    Atom* symbol = ref(tset(new(), T));
+    Atom* Tset = ref(append(symbol, Tget->t));
+    Tset->w = (word) set;
+    Tget->w = (word) get;
+
+    del(Tset);
+    del(symbol);
+    del(ptr);
+    del(valu);
+    del(goer);
+    del(Tget);
+    del(T);
+}
